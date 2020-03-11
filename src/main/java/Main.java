6efa705163f0935d52130a162278c9c7dd97eb1a
AvanNorth <avannorthy@users.noreply.github.com>
2020-03-11 22:16:26 +0300

    че то сделал, сам хз че

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
--- src/main/java/MyCoolBot.java
+++ src/main/java/MyCoolBot.java
@@ -1,4 +1,6 @@
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -11,40 +13,48 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.*;
 
-public class MyCoolBot extends TelegramLongPollingBot {
+public class MyCoolBot extends TelegramLongPollingCommandBot {
 
     InlineKeyboardMarkup inlineKeyboardMarkup =new InlineKeyboardMarkup();
     List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
     List<List<InlineKeyboardButton>> rowList= new ArrayList<>();
+    ArrayList<String> regInfo = new ArrayList<>();
+    private static final String BOT_NAME = "AlbertLOH";
+
+    public MyCoolBot(){
+
+        HelpCommand helpCommand = new HelpCommand(this);
+        register(helpCommand);
+        RegCommand regCommand = new RegCommand(this);
+        register(regCommand);
+    }
 
 
-
-        public void onUpdateReceived(Update update) {
-            if (update.hasMessage() && update.getMessage().hasText()) {
-                if(update.getMessage().getText().equals("Hello")){
-                    try {
-                        execute(sendInlineKeyBoardMessage(update.getMessage().getChatId(),update.getMessage()));
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }else if(update.hasCallbackQuery()){
-                try {
-                    execute(new SendMessage().setText(
-                            update.getCallbackQuery().getData())
-                            .setChatId(update.getCallbackQuery().getMessage().getChatId()));
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-
-        public String getBotUsername() {
+    public String getBotUsername() {
             return "AlbertLOH";
         }
 
-        @Override
+    @Override
+    public void processNonCommandUpdate(Update update) {
+
+        Message msg = update.getMessage();
+        User user = msg.getFrom();
+        SendMessage message = new SendMessage();
+       sendMessageToUser(msg.getChatId(),"Такой команды нет, попробуй /help");
+
+    }
+
+    private void sendMessageToUser(long chatId,String text) {
+        SendMessage message = new SendMessage();
+        message.setText(text);
+        message.setChatId(chatId);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+        }
+    }
+
+    @Override
         public String getBotToken() {
             return "1109980768:AAGW5Xe-_sE5NYqHsH388JnlJBWIifScMXA";
         }
@@ -53,7 +63,7 @@ public class MyCoolBot extends TelegramLongPollingBot {
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
         List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
         //List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
-        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Бог").setCallbackData("Создатель этого бота"));
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Зарегаться").setCallbackData("Создатель этого бота"));
         keyboardButtonsRow1.add(new InlineKeyboardButton().setText("God").setCallbackData("This bot creator"));
         keyboardButtonsRow1.add(new InlineKeyboardButton().setText("UserInfo").setCallbackData("Я знаю многое о тебе, "+ getUserInfo(message,"userName")+" aka " + getUserInfo(message,"firstName")+" "+getUserInfo(message,"lastName")));
        // keyboardButtonsRow2.add(inlineKeyboardButton2);
@@ -73,6 +83,8 @@ public class MyCoolBot extends TelegramLongPollingBot {
             }
             return null;
     }
+
+
 
     }
 
